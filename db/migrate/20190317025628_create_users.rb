f9b0d7ff35ad837bc8a134c80bdcd217b3247e44
masutech16 <leafclover64@gmail.com>
2019-03-24 11:57:39 +0900

    DB migration

--- db/migrate/20190317025628_create_users.rb
+++ db/migrate/20190317025628_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :name, null: false, default: ' '
-      t.string :email, null: false, default: ' '
+      t.string :name, null: false, default: ''
+      t.string :email, null: false, default: ''
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 20190317042902) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name", default: " ", null: false
-    t.string "email", default: " ", null: false
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest", default: " ", null: false
