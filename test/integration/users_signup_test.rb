85a2de36ebcc0f76da148b2933e82a78f20f2f36
masutech16 <leafclover64@gmail.com>
2019-03-24 15:02:26 +0900

    Add signup integration test

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test 'invalid signup information' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: '',
+                                    email: 'user@invalid',
+                                    password: 'foo',
+                                    password_confirmation: 'bar'}}
+    end
+
+    # POST先が正しい
+    assert_select 'form[action=?]', '/signup'
+
+    assert_template 'users/new'
+
+    # error message test
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'div.field_with_errors'
+  end
+end
