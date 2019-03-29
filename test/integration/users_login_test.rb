c8110ac132940910976dfc548795446940c47676
masutech16 <leafclover64@gmail.com>
2019-03-29 23:01:02 +0900

    Implement logout

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,11 +18,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test 'login with valid information' do
+  test 'login with valid information followed by logout' do
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
-
+    assert logged_in?
     assert_redirected_to @user
     follow_redirect!
 
@@ -30,5 +30,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', login_path, count: 0
     assert_select 'a[href=?]', logout_path
     assert_select 'a[href=?]', user_path(@user)
+
+    delete logout_path
+    assert_not logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path,      count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 end
