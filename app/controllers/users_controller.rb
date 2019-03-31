30763374225cf2eaa09596be537119481fd04f1c
masutech16 <leafclover64@gmail.com>
2019-03-31 16:14:38 +0900

    Implement success edit test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # TODO: 更新に成功したときの挙動
+      flash[:success] = 'Profile updated'
+      redirect_to @user
     else
       render 'edit'
     end
