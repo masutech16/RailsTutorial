71d10b8654cff321c8650d8274404f6c16d9fcb3
masutech16 <leafclover64@gmail.com>
2019-04-07 13:38:43 +0900

    Update models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  has_many :microposts
+
   has_secure_password
   before_save { self.email.downcase! }
 
