b8b8d585426c825783c75cfdce040160e2b49285
masutech16 <leafclover64@gmail.com>
2019-06-09 16:46:55 +0900

    Relate Relationship with User

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,8 @@
 class Relationship < ApplicationRecord
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followed, class_name: 'User'
+
+  # Rails 5以降は省略しても動く
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
 
   has_many :microposts, dependent: :destroy
 
+  has_many :active_relationships, class_name: 'Relationship',
+                    foreign_key: 'follower_id',
+                    dependent: :destroy
+
   has_secure_password
   before_save { self.email.downcase! }
 
