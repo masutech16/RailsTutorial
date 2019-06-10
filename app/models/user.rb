c4289a6064f5783e131a4a71e3134d3e5cead28c
masutech16 <leafclover64@gmail.com>
2019-06-10 15:14:02 +0900

    implement user feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,13 @@ class User < ApplicationRecord
     Micropost.where('user_id = ?', id).order(created_at: :desc)
   end
 
+  def feed
+    following_ids = 'SELECT followed_id FROM relationships
+              WHERE follower_id = :user_id'
+    Micropost.where("user_id IN (#{following_ids})
+              OR user_id = :user_id", user_id: id)
+  end
+
   def follow(other_user)
     following << other_user
   end
