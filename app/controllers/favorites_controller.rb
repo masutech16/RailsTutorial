d217ad9246554be5eb30f890deb7314194a13744
masutech16 <leafclover64@gmail.com>
2019-06-23 15:48:13 +0900

    Implement star button

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,18 @@ class FavoritesController < ApplicationController
   before_action :logged_in_user
 
   def create
-    micropost = Micropost.find(params[:micropost_id])
-    current_user.star(micropost)
-    # TODO: どこに飛ばすべきかを考える
+    @micropost = Micropost.find(params[:micropost_id])
+    current_user.star(@micropost)
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
-    micropost = Favorite.find(params[:id]).followed
-    current_user.unstar(micropost)
-    # TODO: どこに飛ばすべきかを考える
+    @micropost = Favorite.find(params[:id]).micropost
+    current_user.unstar(@micropost)
+    respond_to do |format|
+      format.js
+    end
   end
 end
---
+++ test/integration/favorite_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class FavoriteTest < ActionDispatch::IntegrationTest
+    def setup
+    @user = users(:michael)
+    @micropost = microposts(:cat_video)
+    log_in_as(@user)
+  end
+
+  test 'favorite page' do
+    get favorites_user_path(@user)
+    assert_not @user.favorites.empty?
+    assert_match @user.favorites.count.to_s, response.body
+    @user.favorites.each do |micropost|
+      assert_select 'a[href=?]', user_path(micropost.user_id)
+    end
+  end
+
+  # test 'should star a micropost the standard way' do
+  #   assert_difference '@user.favorites.count', 1 do
+  #     post favorites_path, params: { micropost_id: @micropost.id }
+  #   end
+  # end
+
+  test 'should star a micropost with Ajax' do
+    assert_difference '@user.favorites.count', 1 do
+      post favorites_path, xhr: true, params: { micropost_id: @micropost.id }
+    end
+  end
+
+  # test 'should unstar a micropost the standartd way' do
+  #   @user.star(@micropost)
+  #   favorite = @user.favorites.find_by(user_id: @user.id, micropost_id: @micropost.id)
+  #   assert_difference '@user.following.count', -1 do
+  #     delete favorite_path(favorite)
+  #   end
+  # end
+
+  test 'should unstar a micropost with Ajax' do
+    @user.star(@micropost)
+    favorite = @user.favorites.find_by(user_id: @user.id, micropost_id: @micropost.id)
+    assert_difference '@user.favorites.count', -1 do
+      delete favorite_path(favorite), xhr: true
+    end
+  end
+end
